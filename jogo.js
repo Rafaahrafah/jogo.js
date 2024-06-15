ba00ba0f2f4fd5acfc4e9e813cceefae3ed90054
seriouslyvictor <victor.pereira@sp.senai.br>
2024-06-15 13:13:52 -0300

    adicionado logica do jogo

--- jogo.js
+++ jogo.js
@@ -1,7 +1,12 @@
 // Variável de controle para indicar se o jogo está sendo executado/andamento ou parado.
 let executando = false;
+let acertos = 0;
+
 const startBotao = document.querySelector("#btn--start");
 const numSorteado = document.querySelector(".num--atual");
+
+const placarVit = document.querySelector(".num--vit");
+const placarDer = document.querySelector(".num--der");
 
 
 // const start = document.getElementById("btn--start")
@@ -15,11 +20,45 @@ function startGame() {
         return
     } else {
         // Sorteamos um número entre 1 e 20
-        numSorteado.textContent = Math.floor(Math.random()*20+1)
-        executando = true    
+        numSorteado.textContent = Math.floor(Math.random() * 20 + 1)
+        executando = true
     }
 }
 
 function verificarResposta(palpite) {
-    alert(`O palpite foi ${palpite}`)
+    let numAnterior = Number(numSorteado.textContent);
+    numSorteado.textContent = Math.floor(Math.random() * 20 + 1)
+    let numNovo = numSorteado.textContent;
+    // IF!!
+    if (palpite === 'alto' && numNovo > numAnterior) {
+        acertos++;
+        verificarVitoria();
+
+    } else if (palpite === 'baixo' && numNovo < numAnterior) {
+        acertos++;
+        verificarVitoria();
+
+    } else {
+        gameOver()
+    }
+}
+
+function sortearNum() {
+    return numSorteado.textContent = Math.floor(Math.random() * 20 + 1)
+}
+
+function verificarVitoria() {
+    if (acertos >= 3) {
+        placarVit.textContent++;
+
+
+        acertos = 0;
+    }
+}
+
+function gameOver() {
+    placarDer.textContent++;
+    executando = false;
+    numSorteado.textContent = "";
+    acertos = 0;
 }
